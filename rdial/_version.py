865e9b17d9f76bb243e9643fec1432cb7e163ca4
James Rowe <jnrowe@gmail.com>
2012-09-04 15:22:27 +0100

    0.5.1 released.

--- rdial/_version.py
+++ rdial/_version.py
@@ -1,9 +1,9 @@
-# This is rdial version 0.5.0 (2012-08-08)
+# This is rdial version 0.5.1 (2012-09-04)
 # pylint: disable=C0103, C0111, C0121, W0622
 
-dotted = "0.5.0"
-libtool = "5:20"
-hex = 0x000500
-date = "2012-08-08"
-tuple = (0, 5, 0)
-web = "rdial/0.5.0"
+dotted = "0.5.1"
+libtool = "5:21"
+hex = 0x000501
+date = "2012-09-04"
+tuple = (0, 5, 1)
+web = "rdial/0.5.1"
