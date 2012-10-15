b928f5798e90c0a1711c7b693c8176e974729841
James Rowe <jnrowe@gmail.com>
2012-10-15 02:23:34 +0100

    0.6.1 released.

--- rdial/_version.py
+++ rdial/_version.py
@@ -1,9 +1,9 @@
-# This is rdial version 0.6.0 (2012-10-05)
+# This is rdial version 0.6.1 (2012-10-15)
 # pylint: disable=C0103, C0111, C0121, W0622
 
-dotted = "0.6.0"
-libtool = "6:20"
-hex = 0x000600
-date = "2012-10-05"
-tuple = (0, 6, 0)
-web = "rdial/0.6.0"
+dotted = "0.6.1"
+libtool = "6:21"
+hex = 0x000601
+date = "2012-10-15"
+tuple = (0, 6, 1)
+web = "rdial/0.6.1"
