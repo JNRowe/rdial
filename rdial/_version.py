4d92c7e95b151e7f3c44caf5e1a83d97866758ff
James Rowe <jnrowe@gmail.com>
2013-03-24 20:36:13 +0000

    0.11.3 released.

--- rdial/_version.py
+++ rdial/_version.py
@@ -1,9 +1,9 @@
-# This is rdial version 0.11.2 (2013-03-13)
+# This is rdial version 0.11.3 (2013-03-24)
 # pylint: disable=C0103, C0111, C0121, W0622
 
-dotted = '0.11.2'
-libtool = '11:22'
-hex = 0x000b02
-date = '2013-03-13'
-tuple = (0, 11, 2)
-web = 'rdial/0.11.2'
+dotted = '0.11.3'
+libtool = '11:23'
+hex = 0x000b03
+date = '2013-03-24'
+tuple = (0, 11, 3)
+web = 'rdial/0.11.3'
