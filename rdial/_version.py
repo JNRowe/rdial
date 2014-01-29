aaf130f4635526ac89333816a326dffcd4a7b8c7
James Rowe <jnrowe@gmail.com>
2014-01-29 07:06:03 +0000

    Release 0.12.1

--- rdial/_version.py
+++ rdial/_version.py
@@ -1,10 +1,10 @@
-# This is rdial version 0.12.0 (2013-12-28)
+# This is rdial version 0.12.1 (2014-01-29)
 # pylint: disable=C0103, C0111, C0121, W0622
 
-dotted = '0.12.0'
-libtool = '12:20'
-hex = 0x000c00
-date = '2013-12-28'
-tuple = (0, 12, 0)
-dict = {'minor': 12, 'micro': 0, 'major': 0}
-web = 'rdial/0.12.0'
+dotted = '0.12.1'
+libtool = '12:21'
+hex = 0x000c01
+date = '2014-01-29'
+tuple = (0, 12, 1)
+dict = {'major': 0, 'micro': 1, 'minor': 12}
+web = 'rdial/0.12.1'
