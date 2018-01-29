960618246dadb1f6b5f3537983f3570620e6bb95
James Rowe <jnrowe@gmail.com>
2018-01-29 20:28:23 +0000

    [QA] Add SPDX headers
    This is now required by AST's policy bots.

--- rdial/__init__.py
+++ rdial/__init__.py
@@ -18,6 +18,8 @@ A simple time tracking tool, with no frills and no fizzy coating.
 #
 # You should have received a copy of the GNU General Public License along with
 # rdial.  If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0+
 
 from . import _version
 
--- rdial/utils.py
+++ rdial/utils.py
@@ -15,6 +15,8 @@
 #
 # You should have received a copy of the GNU General Public License along with
 # rdial.  If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0+
 
 import configparser
 import datetime
