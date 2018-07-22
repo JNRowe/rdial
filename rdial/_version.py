136b23c7f82259c1f7e7554494d0f3393149e5bf
James Rowe <jnrowe@gmail.com>
2018-07-22 05:17:44 +0100

    Release 0.17.1

--- rdial/_version.py
+++ rdial/_version.py
@@ -1,10 +1,10 @@
-# This is rdial version 0.17.0 (2017-12-14)
+# This is rdial version 0.17.1 (2018-07-22)
 # pylint: skip-file
 
-dotted = '0.17.0'
-libtool = '17:20'
-hex = 0x001100
-date = '2017-12-14'
-tuple = (0, 17, 0)
-dict = {'major': 0, 'minor': 17, 'micro': 0}
-web = 'rdial/0.17.0'
+dotted = '0.17.1'
+libtool = '17:21'
+hex = 0x001101
+date = '2018-07-22'
+tuple = (0, 17, 1)
+dict = {'major': 0, 'minor': 17, 'micro': 1}
+web = 'rdial/0.17.1'
