0e55a1c49baf983038f7e4e7f9141a520c44e6fb
James Rowe <jnrowe@gmail.com>
2018-09-16 06:52:40 +0100

    [Tr] Use HTTPS for jnrbase intersphinx objects

--- doc/conf.py
+++ doc/conf.py
@@ -90,7 +90,7 @@ intersphinx_mapping = {
     k: (v, os.getenv(f'SPHINX_{k.upper()}_OBJECTS'))
     for k, v in {
         'click': 'http://click.pocoo.org/6/',
-        'jnrbase': 'http://jnrbase.readthedocs.io/en/latest/',
+        'jnrbase': 'https://jnrbase.readthedocs.io/en/latest/',
         'python': 'https://docs.python.org/3/',
 }.items()}  # type: Dict[str, str]
 
